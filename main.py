9dad198fdac3920dabd346afe1d289d0f86edc85
Kris98Wolf <kristian98farkas@gmail.com>
2020-02-04 16:15:57 +0100

    mali dodaci

--- main.py
+++ main.py
@@ -1,6 +1,16 @@
+from parser import Parser
+from trie import Trie
+
 def main():
-    ime = input("Kako se zovete?: ")
-    print("Zdravo " + ime)
+    parser = Parser()
+    str = parser.parse('temp/python-2.7.7-docs-html/copyright.html')
+    trie = Trie()
+    for rec in str[1]:
+        trie.add(rec)
+
+    print(trie.find('Python'))
 
 if __name__ == "__main__":
     main()
+
+
