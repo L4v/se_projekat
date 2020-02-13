0cbc5582731aa202159e6738f04a1d8c036371cd
Kris98Wolf <kristian98farkas@gmail.com>
2020-02-13 19:21:02 +0100

    mala izmena kod trie

--- pretraga.py
+++ pretraga.py
@@ -2,17 +2,17 @@ from sets import Set
 
 def pretraga_upita(trie, kriterijum, logical):
     if logical in ['and', 'AND']:
-        return Set(trie.find(kriterijum[0]) * trie.find(kriterijum[1]))
+        return Set(trie.find(kriterijum[0])) * Set(trie.find(kriterijum[1]))
     elif logical in ['or', 'OR']:
-        return Set(trie.find(kriterijum[0]) + trie.find(kriterijum[1]))
+        return Set(trie.find(kriterijum[0])) + Set(trie.find(kriterijum[1]))
     elif logical in ['not', 'NOT']:
-        return Set(trie.find(kriterijum[0]) - trie.find(kriterijum[1]))
+        return Set(trie.find(kriterijum[0])) - Set(trie.find(kriterijum[1]))
     else:
         length = len(kriterijum)
         if length == 1:
             return Set(trie.find(kriterijum[0]))
         else:
-            a = Set(trie.find(kriterijum[0]) + trie.find(kriterijum[1]))
+            a = Set(trie.find(kriterijum[0])) + Set(trie.find(kriterijum[1]))
             if length == 2:
                 return a
             else:
