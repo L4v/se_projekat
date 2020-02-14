825b9884a1a60423a30d5091300ffd490faa2415
Kris98Wolf <kristian98farkas@gmail.com>
2020-02-14 14:16:18 +0100

    mali dodatak

--- unos.py
+++ unos.py
@@ -1,35 +1,41 @@
 from pretraga import pretraga_upita
-from sets import Set
 
 def unos_upita(trie):
-    a = input('Unesite reci za pretragu: ')
 
-    if a == '':
-        print('*** NEMA UNOSA! ***')
-        return Set()
+    while True:
+        a = input('Unesite reci za pretragu: ')
 
-    tokens = a.split()
+        if a == '':
+            print('*** NEMA UNOSA! ***')
+            continue
 
-    kriterijum = []  # reci za pretragu
-    logical = None  # logicki operator
-    count_logical = 0  # samo jedan logicki operator moze biti unet
+        tokens = a.split()
 
-    # proveravamo da li je unet i logicki operator
-    for token in tokens:
-        if token in ['and', 'or', 'not', 'AND', 'OR', 'NOT']:
-            if count_logical == 1:
-                print('* Samo jedan logicki operator mozete uneti! *')
-                return Set()
+        kriterijum = []  # reci za pretragu
+        logical = None  # logicki operator
+        count_logical = 0  # samo jedan logicki operator moze biti unet
+
+        # proveravamo da li je unet i logicki operator
+        for token in tokens:
+            if token in ['and', 'or', 'not', 'AND', 'OR', 'NOT']:
+                if count_logical == 1:
+                    print('* Samo jedan logicki operator mozete uneti! *')
+                    continue
+                else:
+                    logical = token
+                    count_logical = 1
             else:
-                logical = token
-                count_logical = 1
-        else:
-            kriterijum.append(token)
+                kriterijum.append(token)
 
-    # provera unosa
-    if count_logical == 1:
-        if len(kriterijum) != 2 or logical is not tokens[1]:
-            print('*** POGRESAN UNOS! ***')
-            return Set()
+        if logical is tokens[0] and logical in ['not', 'NOT'] and len(kriterijum) == 1:
+            print("Not unos") # zavrsiti
 
-    return pretraga_upita(trie, kriterijum, logical)
+        # provera unosa
+        if count_logical == 1:
+            if len(kriterijum) != 2 or logical is not tokens[1]:
+                print('*** POGRESAN UNOS! ***')
+                continue
+
+
+
+        return pretraga_upita(trie, kriterijum, logical)
