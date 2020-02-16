6069426b8d4e9fccd07d3725a8de6496a7465166
L4v <ivos.jovan@protonmail.ch>
2020-02-16 18:05:01 +0100

    mala izmena

--- unos.py
+++ unos.py
@@ -1,4 +1,5 @@
 from pretraga import pretraga_upita
+from result import Result
 from sets import Set
 
 
@@ -32,7 +33,7 @@ def unos_upita(trie, graph):
         if logical is tokens[0] and logical in ['not', 'NOT'] and len(kriterijum) == 1:
             print("Not unos")
 
-            a = Set(graph.vertices(as_path=True))
+            a = Set([Result(i, 0) for i in graph.vertices(as_path=True)])
             #  trie.find vraca Result tip moramo pretvoriti u path
             #  b ce biti skup stranica koje sadrze tu rec
             b = Set([x.get_path() for x in trie.find(kriterijum[0])])
